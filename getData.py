38846aeb918ad80b1515ba87c9c6ccdb3b640de2
Luiz Carlos Aguiar Carrion <luizcarlosaguiarc@gmail.com>
2021-11-27 02:24:06 -0300

    Testes com o banco de dados

--- getData.py
+++ getData.py
@@ -1,3 +1,19 @@
 import sqlite3
 
-dados = sqlite3.connect('dados.db')
+banco = sqlite3.connect('dados.db')
+cursor = banco.cursor()
+
+cursor.execute("CREATE TABLE alunos (\
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\
+                nome text NOT NULL,\
+                sobrenome text NOT NULL,\
+                email text NOT NULL,\
+                senha text NOT NULL)")
+
+# cursor.execute("INSERT INTO alunos (nome, sobrenome, email, senha) VALUES ('Luiz', 'Carrion', 'teste@gmail.com', '12345')")
+
+# cursor.execute("SELECT * FROM alunos")
+
+# print (cursor.fetchall())
+
+banco.commit()
