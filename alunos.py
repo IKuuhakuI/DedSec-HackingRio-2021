cfa1fbe2df7ec92b614cd667a7ab34caf9926245
Luiz Carlos Aguiar Carrion <luizcarlosaguiarc@gmail.com>
2021-11-27 03:57:42 -0300

    remove os testes dos alunos

--- alunos.py
+++ alunos.py
@@ -7,8 +7,6 @@ def registrarAluno (banco, username, nome, sobrenome, email, senha):
 
     hash = hashlib.sha512(str(senha).encode("utf-8")).hexdigest()
 
-    criou = False
-
     try:
         cursor.execute("INSERT INTO alunos (username, nome, sobrenome, email, senha) VALUES\
                         (?, ?, ?, ?, ?)", (username, nome, sobrenome, email, hash))
@@ -18,11 +16,11 @@ def registrarAluno (banco, username, nome, sobrenome, email, senha):
         criou = True
 
     except:
-        print ("Username já existe!")
+        criou = False
 
-    cursor.execute("SELECT * FROM alunos")
-
-    print(cursor.fetchall())
+    # TESTES PARA VERIFICAR O ESTADO DA CRIAÇÃO #
+    # cursor.execute("SELECT * FROM alunos")
+    # print(cursor.fetchall())
 
     return criou
 
@@ -38,20 +36,14 @@ def validarAluno (banco, username, senha):
         cursor.execute ("SELECT senha FROM alunos WHERE username = (?)", (username,))
         hashReal = cursor.fetchone()
 
-        print (hashReal[0])
-
-        print (hash)
+        # Verifica os valores retornados por cada hash #
+        # print (hashReal[0])
+        # print (hash)
 
         if hashReal[0] == hash:
             return True
         return False
 
     except:
+        # Caso não tenha encontrado o username
         return False
-
-banco = banco.conectar()
-
-if validarAluno (banco, "Mig", "123"):
-    print ("Logado com sucesso")
-else:
-    print ("Usuario ou senha nao existe")
