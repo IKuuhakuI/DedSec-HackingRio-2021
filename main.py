d8751a719300854f257548e1752c4a258560af5a
guilhermeeec <guilhermepic2016@gmail.com>
2021-11-28 00:20:05 -0300

    Tela de avatar iniciada

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
 import banco
 from banco.banco import conectar
 from banco.banco import configurarTabelas
@@ -81,6 +84,17 @@ class TelaLogin(Screen):
         self.senha.text = ""
 
 class TelaAvatar(Screen):
+
+    def desenharCanvas(self):
+        with self.canvas:
+            Color(1,0,0,.5,mode="rgba")
+            x = self.size[0] * 0.5
+            y = self.size[1] * 0.8
+            size_x = self.size[0] * 0.1
+            size_y = self.size[1] * 0.1
+            print(x,y,size_x,size_y)
+            self.rect = Rectangle(pos=(x,y),size=(size_x,size_y))
+
     def confirmar(self,db):
         global usuario_logado
         if True:
